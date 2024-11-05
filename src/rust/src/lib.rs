c8e67cd1e66b0fc04130f0b059d377cfad6eed42
NiklasvonM <niklas.von.moers2@t-online.de>
2024-11-05 21:10:22 +0100

    handle NAs and floats

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -4,19 +4,18 @@ mod jaro;
 mod levenshtein;
 
 //use crate::hamming::hamming_distance;
-use crate::common::FastVec;
 use crate::hamming::hamming_distance_;
 use crate::jaro::{jaro_similarity_, jaro_winkler_similarity_};
 use crate::levenshtein::{damerau_levenshtein_distance_, levenshtein_distance_};
 use extendr_api::prelude::*;
 
-// Macro to turn scalar functions into vectorized ones.
+// Generic macro to turn scalar functions into vectorized ones.
 macro_rules! make_vector_fn {
-    ($fn_name:ident, $vec_fn_name:ident, $ret_type:ty) => {
+    ($fn_name:ident, $vec_fn_name:ident, $ret_type:ty, $convert_fn:expr) => {
         #[extendr]
         /// @export
         fn $vec_fn_name(s1: Strings, s2: Strings) -> Result<Vec<$ret_type>> {
-            let mut result: FastVec<$ret_type> = FastVec::new();
+            let mut result: Vec<$ret_type> = Vec::new();
             let (longer, shorter) = if s1.len() > s2.len() {
                 (s1, s2)
             } else {
@@ -35,25 +34,43 @@ macro_rules! make_vector_fn {
 
             let shorter_vec: Vec<&str> = shorter.iter().map(|rstr| rstr.as_str()).collect();
             let shorter_repeated = shorter_vec.iter().cycle().take(longer.len());
-            // TODO: Handle NAs
+
             for (elem1, elem2) in longer.iter().zip(shorter_repeated) {
-                let val = $fn_name(&elem1.as_str(), &*elem2);
-                result.push(val);
+                result.push(if elem1.is_na() || elem2.is_na() {
+                    <$ret_type>::na()
+                } else {
+                    $convert_fn($fn_name(&elem1.as_str(), &*elem2))
+                });
             }
-            Ok(result.into_vec())
+            Ok(result)
         }
     };
 }
 
+fn to_rint(val: usize) -> Rint {
+    Rint::from(val as i32)
+}
+
+fn to_rfloat(val: f64) -> Rfloat {
+    Rfloat::from(val)
+}
+
+// Now we can use the generic macro for both integer and float functions
 make_vector_fn!(
     damerau_levenshtein_distance_,
     damerau_levenshtein_distance,
-    usize
+    Rint,
+    to_rint
 );
-make_vector_fn!(jaro_similarity_, jaro_similarity, f64);
-make_vector_fn!(jaro_winkler_similarity_, jaro_winkler_similarity, f64);
-make_vector_fn!(hamming_distance_, hamming_distance, usize);
-make_vector_fn!(levenshtein_distance_, levenshtein_distance, usize);
+make_vector_fn!(jaro_similarity_, jaro_similarity, Rfloat, to_rfloat);
+make_vector_fn!(
+    jaro_winkler_similarity_,
+    jaro_winkler_similarity,
+    Rfloat,
+    to_rfloat
+);
+make_vector_fn!(hamming_distance_, hamming_distance, Rint, to_rint);
+make_vector_fn!(levenshtein_distance_, levenshtein_distance, Rint, to_rint);
 
 // Macro to generate exports.
 // This ensures exported functions are registered with R.
